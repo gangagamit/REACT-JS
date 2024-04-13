b80f2a8ec925812f25bfe515cd8b04c9f690fff2
gangagamit <gangagamit350@gmail.com>
2024-04-13 22:49:45 +0530

    13-4-2024

---
+++ Lecture-10/lec10/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Usestate from './Component/UseState';
+import './Style.css';
+function App() {
+  return (
+    <div className="App">
+        <Usestate/>
+    </div>
+  );
+}
+
+export default App;
