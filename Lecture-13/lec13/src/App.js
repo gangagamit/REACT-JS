9795be386a463449f4b12f208ec610d97bc761d9
gangagamit <gangagamit350@gmail.com>
2024-04-14 13:10:26 +0530

    14-4-2024

---
+++ Lecture-13/lec13/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import ClassConpo from './Componenet/ClassCompo';
+function App() {
+  return (
+    <div className="App">
+      <ClassConpo/>
+    </div>
+  );
+}
+
+export default App;
