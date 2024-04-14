c7e49296e31c33da2cb91d98ae02079920afa1b1
gangagamit <gangagamit350@gmail.com>
2024-04-14 13:22:16 +0530

    14-4-2024

---
+++ Lecture-14/lec14/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import Useeffect from './UseEffect/Useeffect';
+function App() {
+  return (
+    <div className="App">
+      <Useeffect/>
+    </div>
+  );
+}
+
+export default App;
