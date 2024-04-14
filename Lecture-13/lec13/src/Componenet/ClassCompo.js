e831aa18e75bcc707c9f23e5b0ce03a6e9cefd2d
gangagamit <gangagamit350@gmail.com>
2024-04-14 13:12:21 +0530

    14-4-2024

--- Lecture-13/lec13/src/Componenet/ClassCompo.js
+++ Lecture-13/lec13/src/Componenet/ClassCompo.js
@@ -10,6 +10,10 @@ export default class ClassConpo extends Component {
             console.log("incr");
             this.setState({count:this.state.count+5})
         }
+        dcr = ()=>{
+            console.log("dcr");
+            this.setState({count:this.state.count-1})
+        }
     
   render() {
     return (
@@ -17,6 +21,7 @@ export default class ClassConpo extends Component {
         <h2>Class component</h2>
         <h2>{this.state.count}</h2>
         <button onClick={this.incr}>click Me</button>
+        <button onClick={this.dcr}>click Me</button>
       </div>
     )
   }
