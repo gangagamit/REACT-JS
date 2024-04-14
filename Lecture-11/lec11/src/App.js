d38468e7e18109386fc98c37933cd7e8c7d8f357
gangagamit <gangagamit350@gmail.com>
2024-04-14 12:56:14 +0530

    14-4-2024

---
+++ Lecture-11/lec11/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Props from './Componenet/Props';
+function App() {
+  return (
+    <div className="App">
+      
+      <Props title="props" des="note that the development build is not optimize"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Lecture-11/lec11/src/Componenet/Props.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Props(xyz) {
+  return (
+    <div>
+      <h1>{xyz.title}</h1>
+      <p>{xyz.des}</p>
+    </div>
+  )
+}
+
+export default Props
