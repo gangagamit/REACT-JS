ce46eda0c01b0c0fc694277f1ff82e01f0665ba9
gangagamit <gangagamit350@gmail.com>
2024-06-19 20:13:23 +0530

    19-6-2024

--- ecommerce/web/src/App.js
+++ ecommerce/web/src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
 import Header from './Component/Header/Header';
-
+import Animation from './Component/Header/Animation/Animation';
 function App() {
   return (
     <div className="App">
         <Header/>
+        <Animation/>
     </div>
   );
 }
---
+++ ecommerce/web/src/Component/Header/Animation/Animation.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Animation() {
+  return (
+    <div>
+      <ScrollAnimation animateIn='bounceInRight'
+  animateOut='bounceOutLeft'>
+  <h2>
+    <a href='https://daneden.github.io/animate.css/'>
+      Animate.css
+    </a>
+  </h2>
+</ScrollAnimation>
+
+    </div>
+  )
+}
+
+export default Animation
